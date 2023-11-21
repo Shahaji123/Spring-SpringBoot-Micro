a9b76de97c2f6de287eba7be7c9070eff32a9a5a
Shahaji123 <jshahaji2511@gmail.com>
2023-11-21 10:21:34 +0530

    commit

---
+++ Quetion-1/src/main/java/com/dizitiveit/hrms/Quetion1Application.java
@@ -0,0 +1,13 @@
+package com.dizitiveit.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quetion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quetion1Application.class, args);
+	}
+
+}
